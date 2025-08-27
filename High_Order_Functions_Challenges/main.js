684a7587e4ecbe5e67383b83fc1eabb7378f5fdc
ElhassnAli <elassn57@gmail.com>
2025-08-27 08:12:08 +0300

    Add files via upload

---
+++ High_Order_Functions_Challenges/main.js
@@ -0,0 +1,36 @@
+/*
+  Higher Order Functions Challenges
+
+  You Can Use
+  - ,
+  - _
+  - Space
+  - True => 1 => One Time Only In The Code
+
+  You Cannot Use
+  - Numbers
+  - Letters
+
+  - You Must Use [Filter + Map + Reduce + Your Knowledge]
+  - Order Is Not Important
+  - All In One Chain
+
+*/
+
+let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+console.log(myString.length);
+
+let solution = myString
+  .split(",")
+  .filter(function (el) {
+    return isNaN(el);
+  })
+  .map(function (el) {
+    return el === "_" ? " " : el;
+  })
+  .reduce(function (let, cur) {
+    return `${let}${cur}`;
+  })
+  .slice(true, -isNaN(myString));
+
+console.log(solution); // Elzero Web School
